24941d5ba833e878558aec3afb4043e54169dd55
ComputronicsWebTeam <computronics.info@gmail.com>
2025-01-10 19:35:48 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ const app = express()
 const PORT = process.env.PORT || 3000
 
 // Middlewares and Important Variables:
-app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
-
+app.use(express.static('public'));
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
 
 app.set('view engine', 'ejs');
